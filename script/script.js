ac83a296190cd9d4e625e4a121e0242baa7ea57f
Alex Garcia del Valle <35402198+xAlpolox@users.noreply.github.com>
2022-07-23 19:28:56 +0100

    ...

---
+++ script/script.js
@@ -0,0 +1,11 @@
+twttr.widgets.createShareButton(
+    "https:\/\/dev.twitter.com\/web\/tweet-button",
+    document.getElementById("twitter"),
+    {
+      size: "large",
+      text: "custom share text",
+      hashtags: "example,demo",
+      via: "twitterdev",
+      related: "twitterapi,twitter"
+    }
+  );
